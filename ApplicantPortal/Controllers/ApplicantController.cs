fe6ee7c431ebcf7f7cd9a786b773cb5b10899975
developerludo <tannaabhishek16@gmail.com>
2019-10-03 09:03:21 +0530

    commit new

--- ApplicantPortal/Controllers/ApplicantController.cs
+++ ApplicantPortal/Controllers/ApplicantController.cs
@@ -1,9 +1,12 @@
 ﻿using ApplicantPortal.Data;
 using ApplicantPortal.Models;
 using ApplicantPortal.Models.StudentDTO;
+using NReco.PdfGenerator;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -222,9 +225,65 @@ namespace ApplicantPortal.Controllers
                         dt.StremId = step3.StreamId;
                         db.AppCollegeMasters.Add(dt);
                         db.SaveChanges();
+                        var data_Application = GetStudentbyId(id);
+                        string htmlString = null;
+                        using (FileStream fs = System.IO.File.Open(HttpContext.Server.MapPath("~/form/form.html"), FileMode.Open, FileAccess.ReadWrite))
+                        {
+                            using (StreamReader sr = new StreamReader(fs))
+                            {
+                                htmlString = sr.ReadToEnd();
+                            }
+                        }
+                        //GenrateHtmlTPDF(htmlString, data_Application);
+                        //byte[] bytes = Encoding.Default.GetBytes(htmlString);
+                        var html = htmlString;
+                        html = html.Replace("@@college1", "3");
+                        html = html.Replace("@@college2", "1");
+                        html = html.Replace("@@college3", "0");
+                        html = html.Replace("@@college4", "1");
+                        html = html.Replace("@@college5", "1");
+                        var RegNo = app.ApplicationNumber.ToString().Select(x => new string(x, 1)).ToArray();
+                        for (var i = 0; i < RegNo.Count(); i++)
+                        {
+                            html = html.Replace("@@Reg" + (i+1), RegNo[i]);
+                        }
+                        html = html.Replace("@@RegisterationNo", app.RollNumber.ToString());
+                        html = html.Replace("@@CollegeName", "L.S COLLEGE,MUZZAFARPUR");
+                        html = html.Replace("@@StudentName", step1.ApplicantName);
+                        html = html.Replace("@@FatherName", step1.FatherName);
+                        html = html.Replace("@@MotherName", step1.MotherName);
+                        html = html.Replace("value="+step2.GenderId, "value=" + step2.GenderId+" checked='checked'");
+                        html = html.Replace("value=Cast" + step2.Cast, "value=Cast" + step2.Cast + " checked='checked'");
+                        var res = step2.MobileNo.Select(x => new string(x, 1)).ToArray();
+                        for(var i = 0; i < res.Count(); i++)
+                        {
+                            html = html.Replace("@@Mobile"+i, res[i]);
+                        }
+                        html = html.Replace("@@Email", step2.Email);
+                        var Rcode = step1.RollCode.Select(x => new string(x, 1)).ToArray();
+                        for (var i = 0; i < Rcode.Count(); i++)
+                        {
+                            html = html.Replace("@@RollCode" + i, Rcode[i]);
+                        }
+                        var RNumber = step1.RollNumber.Select(x => new string(x, 1)).ToArray();
+                        for (var i = 0; i < RNumber.Count(); i++)
+                        {
+                            html = html.Replace("@@RollNumber" + i, RNumber[i]);
+                        }
+                        var year = db.YearMasters.Where(a => a.Year == step1.PassingYearId).FirstOrDefault().Year.ToString();
+                        var sYear= year.Select(x => new string(x, 1)).ToArray();
+                        for (var i = 0; i < sYear.Count(); i++)
+                        {
+                            html = html.Replace("@@PYear" + i, sYear[i]);
+                        }
+                        byte[] bytes = Encoding.Default.GetBytes(html);
+                        TempData["html"] = html;
+                        TempData.Keep();
+                        // System.IO.File.Create(HttpContext.Server.MapPath("~/applicant/data.pdf"));
+                        System.IO.File.WriteAllBytes(HttpContext.Server.MapPath("~/applicant/data1.pdf"), bytes);
                     }
 
-                    return Json(true, JsonRequestBehavior.AllowGet);
+                    return Json(new { result = true, path = "data1.pdf" }, JsonRequestBehavior.AllowGet);
                 }
             }
             catch (Exception ex)
@@ -234,6 +293,76 @@ namespace ApplicantPortal.Controllers
             }
             return Json(false, JsonRequestBehavior.AllowGet);
 
+        }
+
+      
+        public ApplicationMaster GetStudentbyId(int id)
+        {
+
+            try
+            {
+                StudAppDBEntities db = new StudAppDBEntities();
+                var data = db.ApplicationMasters.Where(a => a.ApplicationId == id).FirstOrDefault();
+                return data;
+            }
+            catch (Exception)
+            {
+
+                return (dynamic)null;
+            }
+        }
+
+        public string GenrateHtmlTPDF(string html,ApplicationMaster data)
+        {
+            try
+            {
+                //if (System.IO.File.Exists(HttpContext.Server.MapPath("~/applicant/data.pdf"))){
+                //    System.IO.File.Delete(HttpContext.Server.MapPath("~/applicant/data.pdf"));
+                //}
+                //System.IO.File.Create(HttpContext.Server.MapPath("~/applicant/data.pdf"));
+               
+                Download(HttpContext.Server.MapPath("~/applicant/data.pdf"));
+                return "true";
+            }
+            catch (Exception ex)
+            {
+
+                return "false";
+            }
+        }
+        
+        public FileResult Download(string path)
+        {
+            var htmlToPdf = new HtmlToPdfConverter();
+
+            var pdfContentType = "application/pdf";
+            TempData.Keep();
+            var sData = (dynamic)null;
+            if (Session["sData"] == null)
+            {
+                 sData = TempData["html"];
+                Session["sData"] = sData;
+                return File(htmlToPdf.GeneratePdf(sData, null), pdfContentType);
+            }
+            else
+            {
+                sData=Session["sData"] ;
+                return File(htmlToPdf.GeneratePdf(sData, null), pdfContentType);
+            }
+           
+            
+            //byte[] fileBytes = System.IO.File.ReadAllBytes(HttpContext.Server.MapPath("~/applicant/"+path));
+            //string fileName = path;
+            //MemoryStream workStream = new MemoryStream();
+            //workStream.Write(fileBytes, 0, fileBytes.Length);
+            //workStream.Position = 0;
+            //return File(workStream.ToArray(), "application/pdf", fileName);
+            //using (MemoryStream pdfStream = new MemoryStream())
+            //{
+            //    pdfStream.Write(fileBytes, 0, fileBytes.Length);
+            //    pdfStream.Position = 0;
+            //    return new FIle(pdfStream, "application/pdf");
+            //}   
         }
     }
 }
